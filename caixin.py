e761b1649af1618cc095b260a1b5d771c1bfc4b0
xwt <93423084+WentongXie@users.noreply.github.com>
2025-10-24 09:46:44 +0800

    format code

--- caixin.py
+++ caixin.py
@@ -1,4 +1,9 @@
-import requests, logging, json, os, urllib, time, traceback
+import requests
+import logging
+import os
+import urllib
+import time
+import traceback
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,8 +12,9 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class article:
-    def __init__(self, article_id:str, title:str, href:str):
+    def __init__(self, article_id: str, title: str, href: str):
         self.article_id = article_id.strip()
         self.title = title.strip()
         self.href = href.strip()
@@ -16,16 +22,20 @@ class article:
     def __str__(self):
         return str(self.__dict__)
 
+
 template = ""
 header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-        }
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+}
+
 
 def main():
     name = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-    logging.basicConfig(filename = name + ".txt",level = logging.INFO, format = LOG_FORMAT, encoding = 'utf-8')
+    logging.basicConfig(filename=name + ".txt",
+                        level=logging.INFO, format=LOG_FORMAT, encoding='utf-8')
     pass
+
 
 '''
 def download_total(session:requests.Session):
@@ -71,11 +81,12 @@ def download_article(session:requests.Session, path, article_id, title):
         f.write(template.format(title = title, content = str(soup)))
 '''
 
-def download_img(url, file_path, cookie = None, session = None) -> None:
+
+def download_img(url, file_path, cookie=None, session=None) -> None:
     url = url.strip()
     urlparse = urllib.parse.urlparse(url)
     if urlparse.scheme == "":
-        urlparse = urlparse._replace(scheme = 'https')
+        urlparse = urlparse._replace(scheme='https')
         url = urllib.parse.urlunparse(urlparse)
     if session:
         response = session.get(url)
@@ -90,18 +101,20 @@ def download_img(url, file_path, cookie = None, session = None) -> None:
     with open(file_path, "wb") as f:
         f.write(response.content)
 
+
 def download_articles(article_list: list[article], dir_path: str) -> None:
     ser = webdriver.ChromeService(executable_path="chromedriver.exe")
-    #ser = webdriver.EdgeService(executable_path="msedgedriver.exe")
-    #options = webdriver.EdgeOptions()
+    # ser = webdriver.EdgeService(executable_path="msedgedriver.exe")
+    # options = webdriver.EdgeOptions()
     options = webdriver.ChromeOptions()
     user_data_dir = os.path.join(os.getcwd(), "UserData")
     os.makedirs(user_data_dir, exist_ok=True)
     options.add_argument("user-data-dir={}".format(user_data_dir))
     try:
         driver = webdriver.Chrome(service=ser, options=options)
-        #driver = webdriver.Edge(service=ser)
-        driver.get("https://u.caixin.com/web/login?url=https%3A%2F%2Fweekly.caixin.com%2F")
+        # driver = webdriver.Edge(service=ser)
+        driver.get(
+            "https://u.caixin.com/web/login?url=https%3A%2F%2Fweekly.caixin.com%2F")
         time.sleep(60)
         for article in article_list:
             logging.info(article)
@@ -111,16 +124,17 @@ def download_articles(article_list: list[article], dir_path: str) -> None:
         driver.quit()
     pass
 
+
 def download_article(driver: webdriver.Chrome, article: article, dir_path: str) -> None:
     try:
-        content = getContent(driver, "{}?p0".format(article.href))
+        content = getContent(driver, "{}?p0".format(article.href.strip()))
     except:
         logging.error("Exception: %s", traceback.format_exc())
         logging.error(article)
         return
     os.makedirs(dir_path, exist_ok=True)
     soup = BeautifulSoup(content, 'html.parser')
-    aitt = soup.find_all("p", class_ = "aitt")
+    aitt = soup.find_all("p", class_="aitt")
     for i in aitt:
         i.decompose()
     imgs = soup.find_all("img")
@@ -131,7 +145,8 @@ def download_article(driver: webdriver.Chrome, article: article, dir_path: str) 
             img["src"] = "/favicon.ico"
             continue
         urlparse = urllib.parse.urlparse(src)
-        basename = "{}_{}".format(article.article_id, os.path.basename(urlparse.path))
+        basename = "{}_{}".format(
+            article.article_id, os.path.basename(urlparse.path))
         basename = basename.strip()
         img_path = os.path.join(dir_path, basename)
         try:
@@ -143,7 +158,8 @@ def download_article(driver: webdriver.Chrome, article: article, dir_path: str) 
         img["src"] = urllib.request.pathname2url(basename)
     file_name = os.path.join(dir_path, "{}.html".format(article.article_id))
     with open(file_name, "w", encoding="utf") as f:
-        f.write(template.format(title = article.title, content = str(soup)))
+        f.write(template.format(title=article.title, content=str(soup)))
+
 
 def getContent(driver: webdriver.Chrome, url: str) -> str:
     url = url.strip()
@@ -152,27 +168,30 @@ def getContent(driver: webdriver.Chrome, url: str) -> str:
         WebDriverWait(driver, 60).until(
             EC.presence_of_element_located((By.CLASS_NAME, "end_ico"))
         )
-    except TimeoutException: #handle WebDriverWait
-        logging.warning("TimeoutException: end_ico, url: {}".format(driver.current_url))
+    except TimeoutException:  # handle WebDriverWait
+        logging.warning(
+            "TimeoutException: end_ico, url: {}".format(driver.current_url))
         try:
             driver.find_element(By.LINK_TEXT, "余下全文").click()
             WebDriverWait(driver, 60).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "end_ico"))
             )
-        except NoSuchElementException: #handle driver.find_element
+        except NoSuchElementException:  # handle driver.find_element
             pass
-        except TimeoutException: #handle WebDriverWait
-            logging.warning("TimeoutException: left end_ico, url: {}".format(driver.current_url))
+        except TimeoutException:  # handle WebDriverWait
+            logging.warning(
+                "TimeoutException: left end_ico, url: {}".format(driver.current_url))
     content = driver.find_element(By.ID, "Main_Content_Val")
     return content.get_attribute("innerHTML")
 
-def download_magazine(session:requests.Session, url:str, magazine_dir_path:str) -> list[article]:
+
+def download_magazine(session: requests.Session, url: str, magazine_dir_path: str) -> list[article]:
     rsp = session.get(url)
     soup = BeautifulSoup(rsp.text, 'html.parser')
-    main_conntent = soup.find("div", class_ = "mainMagContent")
-    title = main_conntent.find("div", class_ = "title").get_text()
-    main_conntent.find("div", class_ = "date").decompose()
-    cover = main_conntent.find("div", class_ = "cover")
+    main_conntent = soup.find("div", class_="mainMagContent")
+    title = main_conntent.find("div", class_="title").get_text()
+    main_conntent.find("div", class_="date").decompose()
+    cover = main_conntent.find("div", class_="cover")
     img = cover.find("img")
     src = img.get("src")
     src.strip()
@@ -192,12 +211,14 @@ def download_magazine(session:requests.Session, url:str, magazine_dir_path:str) 
             continue
         href = href.strip()
         basename = os.path.basename(urllib.parse.urlparse(href).path)
-        article_list.append(article(os.path.splitext(basename)[0], i.get_text(), href))
+        article_list.append(
+            article(os.path.splitext(basename)[0], i.get_text(), href))
         i["href"] = urllib.request.pathname2url(basename)
     with open(os.path.join(magazine_dir_path, "index.html"), "w", encoding="utf") as f:
-        f.write(template.format(title = title, content = str(main_conntent)))
+        f.write(template.format(title=title, content=str(main_conntent)))
     logging.debug(article_list)
     return article_list
+
 
 if __name__ == "__main__":
     main()
